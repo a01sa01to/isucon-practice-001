d25771636af844c59bafcf8ca6834a267e0ff95b
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-02-10 23:14:25 +0900

    k6: check commenting

--- webapp/k6/comment.js
+++ webapp/k6/comment.js
@@ -8,8 +8,24 @@ export default function () {
     account_name: Username,
     password: Password,
   });
-  console.log(`loginRes: ${JSON.stringify(loginRes)}`);
   check(loginRes, {
-    "is status 200": (r) => r.status === 200,
+    "successfully logged in": (r) => r.status === 200,
+  });
+
+  const res = http.get(`${BaseUrl}/@${Username}`)
+
+  const doc = parseHTML(res.body);
+
+  const csrf_token = doc.find('input[name=csrf_token]').first().attr('value')
+  const post_id = doc.find('input[name=post_id]').first().attr('value')
+
+  const comment_res = http.post(`${BaseUrl}/comment`, {
+    post_id,
+    csrf_token,
+    comment: "Hello, k6!",
+  });
+
+  check(comment_res, {
+    "successfully commented": (r) => r.status === 200,
   });
 }
