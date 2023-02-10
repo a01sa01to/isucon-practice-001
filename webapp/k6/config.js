a17cc422906326cc7c01931e5ad53b180919a694
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-02-10 23:43:41 +0900

    Impl getRandomAccount

--- webapp/k6/config.js
+++ webapp/k6/config.js
@@ -2,3 +2,9 @@ export const BaseUrl = "http://localhost:80";
 export const Username = "asa";
 export const Password = "asasasa";
 export const TargetUsername = "mary"
+
+export const getRandomAccount = () => {
+  const accounts = ["mary", "jordan", "lula", "mai", "julia"];
+  const name = accounts[Math.floor(Math.random() * accounts.length)];
+  return [name, name + name];
+}
--- webapp/k6/postimage.js
+++ webapp/k6/postimage.js
@@ -1,11 +1,12 @@
 import http from "k6/http";
-import { BaseUrl, Username, Password } from "./config.js";
+import { BaseUrl, getRandomAccount } from "./config.js";
 import { check } from "k6";
 import { parseHTML } from "k6/html";
 
 const testimage = open("./testimage.png", "b");
 
 export default function () {
+  const [Username, Password] = getRandomAccount();
   const loginRes = http.post(`${BaseUrl}/login`, {
     account_name: Username,
     password: Password,
